191b02b53314f36e05115d653b57b25175de12d9
Christopher Reid <christopherreid@Christophers-MacBook-Pro.local>
2025-05-08 23:37:17 +0200

    Initial plugin setup for Sandbaai Crime Tracker

--- includes/class-sandbaai-crime-statistics-dashboard.php
+++ includes/class-sandbaai-crime-statistics-dashboard.php
@@ -13,16 +13,16 @@ class Sandbaai_Crime_Statistics_Dashboard {
     /**
      * Add the statistics page to the admin menu.
      */
-    public function add_statistics_page() {
-        add_submenu_page(
-            'sandbaai-crime-tracker', // Parent menu slug
-            'Crime Statistics',       // Page title
-            'Crime Statistics',       // Menu title
-            'manage_options',         // Capability
-            'sandbaai-crime-statistics', // Menu slug
-            [$this, 'render_statistics_page'] // Callback function
-        );
-    }
+public function add_statistics_page() {
+    add_submenu_page(
+        'sandbaai-crime-tracker', // Parent menu slug
+        'Crime Statistics',       // Page title
+        'Crime Statistics',       // Menu title
+        'manage_options',         // Capability
+        'sandbaai-crime-tracker-crime-statistics', // Menu slug
+        [$this, 'render_statistics_page'] // Callback function
+    );
+}
 
     /**
      * Render the statistics page.
